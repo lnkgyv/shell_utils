3c236f2d3a0fb31d1a0b96ae9cf041cff1e9f2da
Yurij Goncharuk <goncharuk@nicevt.ru>
2022-09-27 19:32:40 +0300

    numeral_system_converter: initial commit

---
+++ numeral_system_converter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+###############################################################################
+#
+# Numeral system converter convertes number or list of comma separate number
+# from specific numeral system(bases: 8(0 prefix), 16(0x prefix)) to decimal.
+#
+# numeral_system_converter.sh <input file>
+#
+###############################################################################
+
+if [ -z "$1" ] ;
+then
+	echo "There is no enought parameter:"
+	echo "$0 <input file(or stdin)>"
+
+	exit 0
+fi
+
+while read -r -d, number
+do
+	printf "%d," "$number"
+done < "$1"
+printf "%d \b \n" "$number"
